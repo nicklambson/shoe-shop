a1d50365997fba58bca2c0cd000292b11fc807c2
Nick Lambson <nicklambson@gmail.com>
2021-05-19 19:09:00 +0800

    Updates

--- shop.py
+++ shop.py
@@ -6,24 +6,50 @@ import csv
 import gettext
 from collections import defaultdict
 
-def sel():
+def select_shoes():
+    # refresh variables
     my_language = language.get()
+    my_choice = choice.get()
+    
+    # set the locale
     locale_functions[my_language].install()
     _ = locale_functions[my_language].gettext
     locale.setlocale(locale.LC_ALL, my_language)
-    choice = var.get()
-
+    
+    # get the price
     exchange_rate = my_prices["USD"][my_locales[language.get()]]
-    USD_price = choice_to_prices[var.get()]
-    print(exchange_rate)
-    print(USD_price)
+    USD_price = choice_to_prices[choice.get()]
     final_price = USD_price * exchange_rate
-    formatted_price = locale.currency(final_price, grouping=True)
-    price_message = _(f"The price of your selection is {formatted_price}.")
-    delivery_days = choice_to_delivery_days[choice]
-    arriving = _(f"Your order will arrive in {delivery_days} days.")
+    formatted_price = locale.currency(final_price, grouping=True, international=True)
+
+    # get delivery days
+    delivery_days = choice_to_delivery_days[my_choice]
+
+    # set up the messages
+    price_message = _("The price of your selection is %(price)s.") % {'price' : formatted_price}
+    arriving = _("Your order will arrive in %(days)s days.") % {'days' : delivery_days}
     total_message = " ".join([price_message, arriving])
     message.config(text = total_message)
+
+    # refresh all prices on buttons
+    price_1_USD = choice_to_prices[1]
+    price_1_final = price_1_USD * exchange_rate
+    price_1_formatted = locale.currency(price_1_final, grouping=True, international=True)
+    price_1 = tk.Label(photo_1_frame, text=price_1_formatted, compound="top", bd=5)
+    price_1.pack(side=tk.BOTTOM)
+
+    price_2_USD = choice_to_prices[2]
+    price_2_final = price_2_USD * exchange_rate
+    price_2_formatted = locale.currency(price_2_final, grouping=True, international=True)
+    price_2 = tk.Label(photo_2_frame, text=price_2_formatted, compound="top", bd=5)
+    price_2.pack(side=tk.BOTTOM)
+
+    price_3_USD = choice_to_prices[3]
+    price_3_final = price_3_USD * exchange_rate
+    price_3_formatted = locale.currency(price_3_final, grouping=True, international=True)
+    price_3 = tk.Label(photo_3_frame, text=price_3_formatted, compound="top", bd=5)
+    price_3.pack(side=tk.BOTTOM)
+
 
 def read_forex(path):
     my_prices = defaultdict(dict)
@@ -35,23 +61,43 @@ def read_forex(path):
                 my_prices[row[0]].update({data[0][col] : float(row[col])})
     return my_prices
 
+en = gettext.translation('es_MX',
+                         localedir='locale',
+                         languages=['es'],
+                         fallback=True)
+zh = gettext.translation('zh_CN',
+                         localedir='locale',
+                         languages=['zh'],
+                         fallback=True)
+ru = gettext.translation('ru_RU',
+                         localedir='locale',
+                         languages=['ru'],
+                         fallback=True)
 es = gettext.translation('es_MX',
                          localedir='locale',
                          languages=['es'],
                          fallback=True)
-locale_functions = {
-    "es_MX" : es
-}
+fr = gettext.translation('fr_FR',
+                         localedir='locale',
+                         languages=['fr'],
+                         fallback=True)
+ko = gettext.translation('ko_KR',
+                         localedir='locale',
+                         languages=['ko'],
+                         fallback=True)
+ja = gettext.translation('ja_JP',
+                         localedir='locale',
+                         languages=['ja'],
+                         fallback=True)
 
-options = [
-    "en_US",
-    "zh_CN",
-    "ru_RU",
-    "es_MX",
-    "fr_FR",
-    "ko_KR",
-    "ja_JP",
-]
+locale_functions = {
+    "en_US" : en,
+    "zh_CN" : zh,
+    "ru_RU" : ru,
+    "es_MX" : es,
+    "fr_FR" : fr,
+    "ko_KR" : ko,
+    "ja_JP" : ja}
 
 my_locales = {
     "en_US" : "USD",
@@ -60,20 +106,17 @@ my_locales = {
     "es_MX" : "MXN",
     "fr_FR" : "EUR",
     "ko_KR" : "KRW",
-    "ja_JP" : "JPY",
-}
+    "ja_JP" : "JPY",}
 
 choice_to_prices = {
     1 : 19.99,
     2 : 29.99,
-    3 : 14.99,
-}
+    3 : 14.99,}
 
 choice_to_delivery_days = {
     1 : 2,
     2 : 4,
-    3 : 6,
-}
+    3 : 6,}
 
 root = tk.Tk()
 root.withdraw()
@@ -87,14 +130,16 @@ top_bar.pack(side=tk.TOP)
 top = tk.Frame(root, bd=5)
 top.pack(side=tk.TOP)
 
-var = tk.IntVar()
+choice = tk.IntVar()
+
+my_prices = read_forex('forex.csv')
 
 photo_1_frame = tk.Frame(top)
 photo_1_frame.pack(side=tk.LEFT)
 photo_1 = tk.PhotoImage(file="shoes/shoes_1.png")
 photo_1_label = tk.Label(photo_1_frame, image=photo_1, text="Rainbow Stars", compound="top", bd=5)
 photo_1_label.pack(side=tk.TOP)
-R1 = tk.Radiobutton(photo_1_frame, text="Option 1", variable=var, value=1, command=sel)
+R1 = tk.Radiobutton(photo_1_frame, text="Option 1", variable=choice, value=1, command=select_shoes)
 R1.pack(side=tk.BOTTOM)
 price_1 = tk.Label(photo_1_frame, text="$19.99", compound="top", bd=5)
 price_1.pack(side=tk.BOTTOM)
@@ -105,7 +150,7 @@ photo_2_frame.pack(side=tk.LEFT)
 photo_2 = tk.PhotoImage(file="shoes/shoes_2.png")
 photo_2_label = tk.Label(photo_2_frame, image=photo_2, text="Converse", compound="top", bd=5)
 photo_2_label.pack(side=tk.TOP)
-R2 = tk.Radiobutton(photo_2_frame, text="Option 2", variable=var, value=2, command=sel)
+R2 = tk.Radiobutton(photo_2_frame, text="Option 2", variable=choice, value=2, command=select_shoes)
 R2.pack(side=tk.BOTTOM)
 price_2 = tk.Label(photo_2_frame, text="$29.99", compound="top", bd=5)
 price_2.pack(side=tk.BOTTOM)
@@ -116,7 +161,7 @@ photo_3_frame.pack(side=tk.LEFT)
 photo_3 = tk.PhotoImage(file="shoes/shoes_3.png")
 photo_3_label = tk.Label(photo_3_frame, image=photo_3, text="Slip Ons", compound="top", bd=5)
 photo_3_label.pack(side=tk.TOP)
-R3 = tk.Radiobutton(photo_3_frame, text="Option 3", variable=var, value=3, command=sel)
+R3 = tk.Radiobutton(photo_3_frame, text="Option 3", variable=choice, value=3, command=select_shoes)
 R3.pack(side=tk.BOTTOM)
 price_3 = tk.Label(photo_3_frame, text="$14.99", compound="top", bd=5)
 price_3.pack(side=tk.BOTTOM)
@@ -137,14 +182,12 @@ bottom.pack(side=tk.BOTTOM)
 
 language = tk.StringVar()
 language.set("en_US")
-language_menu = tk.OptionMenu(top_bar, language, *options)
+language_menu = tk.OptionMenu(top_bar, language, *locale_functions.keys())
 language_menu.pack(side=tk.LEFT)
 
 message = tk.Label(bottom)
 message.pack()
 
-my_prices = read_forex('forex.csv')
-for start, conversions in my_prices.items():
-    print(start, conversions)
+
 
 tk.mainloop()
---
+++ shop_new.py
@@ -0,0 +1,15 @@
+
+
+# Refreshable text
+# Shoes 1 price
+# Shoes 1 name (on radio button)
+# Shoes 2 price
+# Shoes 2 name (on radio button)
+# Shoes 3 price
+# Shoes 3 name (on radio button)
+# Message at bottom
+# Shoe Shop title
+
+# Refresh on which actions?
+# Selection of new language
+# Selection of new option
